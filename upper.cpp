1d114d25527d35b7460f393ed536b6c1ee32e879
mstr-Wolf <lohan.chuan123@gmail.com>
2020-12-18 14:23:08 -0300

    upper fixed!

--- upper.cpp
+++ upper.cpp
@@ -1,9 +1,20 @@
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
 void conversaoCaixaAlta(char vetor[]){
-  for (size_t i = 0; i < strlen(vetor); i++) {
-      if(vetor[i] >= 'a' && vetor[i] <= 'z') {
-         vetor[i] = *(vetor - 32);
+    //CONVERTE PARA CAIXA ALTA
+    for (size_t i = 0; i < strlen(vetor); i++) {
+        if(vetor[i] >= 'a' && vetor[i] <= 'z') {
+            vetor[i] = toupper(vetor[i]);
+        }
     }
-  }
+    //printf("%s\n", vetor);
 }
+
+/*
+int main(){
+    char nome[7] = "lo han";
+    conversaoCaixaAlta(nome);
+}
+*/
